8240dd96bf3ae1de647d07417144e29f7d09d111
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-06 01:42:43 -0500

    Added methods in routes/api/product-routes.js

--- seeds/index.js
+++ seeds/index.js
@@ -3,10 +3,10 @@ const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const sequelize = require('../config/connection');
+const dbConnector = require('../config/connection');
 
 const seedAll = async () => {
-  await sequelize.sync({ force: true });
+  await dbConnector.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
