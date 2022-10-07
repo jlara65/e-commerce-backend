f15a01354fb1bcc46f2b99a146a48512c4912a8d
Joseph Lara <vipercar233@gmail.comgit config --global user.name Joseph>
2022-10-06 23:41:40 -0500

    Add methods in routes/api/tag-routes.js. fixed the incorrect dotenv name and fixed listen section in server.js

--- seeds/index.js
+++ seeds/index.js
@@ -3,10 +3,10 @@ const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
-const dbConnector = require('../config/connection');
+const sequelize = require('../config/connection');
 
 const seedAll = async () => {
-  await dbConnector.sync({ force: true });
+  await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedCategories();
   console.log('\n----- CATEGORIES SEEDED -----\n');
